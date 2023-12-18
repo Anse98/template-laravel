4365edacbe41a620718426e2c24daa781e385e1d
Anse98 <lorenzo.anselmi98.com>
2023-12-18 13:31:47 +0100

    rimosso controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Guest\PageController as PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', function () {
+    return view('index');
+});
